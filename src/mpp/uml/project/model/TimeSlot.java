07422f59177e903940d1b57733ce66bc02fab0a6
Silas Kaggwa <silakag@gmail.com>
2018-06-04 15:25:53 -0500

    TimeSlot getters and Setter

--- src/mpp/uml/project/model/TimeSlot.java
+++ src/mpp/uml/project/model/TimeSlot.java
@@ -9,4 +9,42 @@ public class TimeSlot {
 	public Date beginTime;
 	public Date endTime;
 	public List<BarcodeRecord> barcodeRecords;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setBeginTime(Date beginTime) {
+		this.beginTime = beginTime;
+	}
+	
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	public void setBarcodeRecords(List<BarcodeRecord> barcodeRecords) {
+		this.barcodeRecords = barcodeRecords;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public Date getBeginTime() {
+		return this.beginTime;
+	}
+	
+	public Date getEndTime() {
+		return this.endTime;
+	}
+	public List<BarcodeRecord> getBarcodeRecords() {
+		return this.barcodeRecords;
+	}
 }
