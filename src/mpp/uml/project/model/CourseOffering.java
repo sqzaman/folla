8aa1452c38ca2a9d4c9c1533c29f7648799acc16
Khangarid <khangarid.davaadorj@gmail.com>
2018-06-04 15:06:45 -0500

    variables

--- src/mpp/uml/project/model/CourseOffering.java
+++ src/mpp/uml/project/model/CourseOffering.java
@@ -1,5 +1,57 @@
 package mpp.uml.project.model;
 
-public class CourseOffering {
+import java.util.ArrayList;
 
+public class CourseOffering {
+	private String id;
+	private Location location;
+	private ArrayList<Session> sessions;
+	private Course course;
+	private PlanOfStudy planOfStudy;
+	private ArrayList<AcademicBlock> academicBlocks;
+	private Faculty faculty;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public ArrayList<Session> getSessions() {
+		return sessions;
+	}
+	public void setSessions(ArrayList<Session> sessions) {
+		this.sessions = sessions;
+	}
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public PlanOfStudy getPlanOfStudy() {
+		return planOfStudy;
+	}
+	public void setPlanOfStudy(PlanOfStudy planOfStudy) {
+		this.planOfStudy = planOfStudy;
+	}
+	public ArrayList<AcademicBlock> getAcademicBlocks() {
+		return academicBlocks;
+	}
+	public void setAcademicBlocks(ArrayList<AcademicBlock> academicBlocks) {
+		this.academicBlocks = academicBlocks;
+	}
+	public Faculty getFaculty() {
+		return faculty;
+	}
+	public void setFaculty(Faculty faculty) {
+		this.faculty = faculty;
+	}
+	
 }
--- src/mpp/uml/project/model/PlanOfStudy.java
+++ src/mpp/uml/project/model/PlanOfStudy.java
@@ -1,5 +1,21 @@
 package mpp.uml.project.model;
 
-public class PlanOfStudy {
+import java.util.ArrayList;
 
+public class PlanOfStudy {
+	private ArrayList<CourseOffering> courseOfferings;
+	private Student student;
+	
+	public ArrayList<CourseOffering> getCourseOfferings() {
+		return courseOfferings;
+	}
+	public void setCourseOfferings(ArrayList<CourseOffering> courseOfferings) {
+		this.courseOfferings = courseOfferings;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
 }
