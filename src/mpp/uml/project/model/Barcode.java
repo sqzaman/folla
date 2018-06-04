1596c79ff76b1efcdfbe592c5d0bdad659a454f6
sqzaman <sqzaman@gmail.com>
2018-06-04 15:11:26 -0500

    updated

--- src/mpp/uml/project/model/Barcode.java
+++ src/mpp/uml/project/model/Barcode.java
@@ -3,4 +3,17 @@ package mpp.uml.project.model;
 public class Barcode {
 	int id;
 	String barcode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	
 }
